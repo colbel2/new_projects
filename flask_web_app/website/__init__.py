aee9a9a3ea76e3e2f632094b0f6ae07cf6beb741
colbel2 <colbel22@gmail.com>
2022-08-15 16:02:18 -0500

    Validations, accesible pages, delete function working

--- flask_web_app/website/__init__.py
+++ flask_web_app/website/__init__.py
@@ -1,26 +1,39 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
+from flask_login import LoginManager
+
+
 db = SQLAlchemy()
 DB_NAME = "database.db"
+
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'secret'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
-    
-    from .views import views #from views file import name of blueprint, views
+
+    from .views import views  # from views file import name of blueprint, views
     from .auth import auth
 
-    app.register_blueprint(views, url_prefix='/')# all the url stored inside file 
+    # all the url stored inside file
+    app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
     from .models import User, Note
     # from .models import User, Note #import .models would work as well.
 
     create_database(app)
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
 
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+    
+    
     return app
 
 def create_database(app):
