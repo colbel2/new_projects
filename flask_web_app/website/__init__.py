49eaf689e92f1a3a9d3117e1c4624394b6f6a0ad
colbel2 <colbel22@gmail.com>
2022-08-14 20:48:36 -0500

    Sign up and login working with db

--- flask_web_app/website/__init__.py
+++ flask_web_app/website/__init__.py
@@ -1,8 +1,14 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'secret'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
     
     from .views import views #from views file import name of blueprint, views
     from .auth import auth
@@ -10,4 +16,14 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')# all the url stored inside file 
     app.register_blueprint(auth, url_prefix='/')
 
+    from .models import User, Note
+    # from .models import User, Note #import .models would work as well.
+
+    create_database(app)
+
     return app
+
+def create_database(app):
+    if not path.exists('website/' + DB_NAME):
+        db.create_all(app=app)
+        print('Created Database!')
